e5395bde90d3d6ca1b07b30106167cc786c65a64
evanchildress <evan.childress@gmail.com>
2016-04-28 17:17:09 -0400

    dealing with some missing values

--- vignettes/westBrook/biomassDataPrep.R
+++ vignettes/westBrook/biomassDataPrep.R
@@ -74,17 +74,12 @@ biomass<-abundance[meanWeight] %>%
   setnames(c("1","2"),c("yoy","adults")) %>%
   .[,totalBiomass:=sum(yoy,adults,na.rm=T),by=.(river,year,season)]
 
-a<-lm(totalBiomass~as.factor(year)*as.factor(season)*river,data=biomass)
-unsampled<-data.table(river="west brook",totalBiomass=NA,season=c(4,4,4,1),
-                      year=c(2005,2007,2012,2015))
-unsampled$totalBiomass<-predict(a,data.frame(unsampled[,.(river,year,season)]))
+unsampled<-data.table(river=c("wb jimmy","west brook",'west brook','west brook','west brook'),
+                      season=c(4,4,4,4,1),
+                      year=c(2002,2005,2007,2012,2015),
+                      totalBiomass=c(2283.985,5000,3000,4000,2200))
 
-unsampled<-
-  rbind(unsampled,
-        biomass[((season==3&year==2002)|(season==1&year==2003))&river!="west brook",
-                .(totalBiomass=mean(totalBiomass)),by=river] %>%
-          .[,":="(season=4,year=2002)]
-  )
+
 
 biomass<-bind_rows(biomass,unsampled) %>% data.frame() %>% data.table() %>%
   .[,.(river,year,season,totalBiomass)] %>%
